a172e96388662edee2b2cce5ded9e9f716b13f91
Mehboob-alam1 <ma8122576@gmail.com>
2023-02-26 18:43:59 +0000

    Initial commit

---
+++ app/src/main/java/com/mehboob/azdawaaj/ui/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.mehboob.azdawaaj.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+
+import com.mehboob.azdawaaj.MainActivity;
+import com.mehboob.azdawaaj.R;
+import com.mehboob.azdawaaj.databinding.ActivityLoginBinding;
+
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+private ActivityLoginBinding binding;
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[@#$%^&+=])" +     // at least 1 special character
+                    "(?=\\S+$)" +            // no white spaces
+                    ".{4,}" +                // at least 4 characters
+                    "$");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLogin.setOnClickListener(v -> {
+confirmInput();
+        });
+    }
+
+    public void confirmInput() {
+        if (!validateEmail() | !validatePassword()) {
+            return;
+        }else{
+
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        }
+
+
+    }
+
+    private boolean validateEmail() {
+
+        // Extract input from EditText
+        String emailInput = binding.etEmailLayout.getEditText().getText().toString().trim();
+
+        // if the email input field is empty
+        if (emailInput.isEmpty()) {
+            binding.etEmailLayout.setError("Field can not be empty");
+            return false;
+        }
+
+        // Matching the input email to a predefined email pattern
+        else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            binding.etEmailLayout.setError("Please enter a valid email address");
+            return false;
+        } else {
+            binding.etEmailLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String passwordInput = binding.etPasswordLayout.getEditText().getText().toString().trim();
+        // if password field is empty
+        // it will display error message "Field can not be empty"
+        if (passwordInput.isEmpty()) {
+            binding.etPasswordLayout.setError("Field can not be empty");
+            return false;
+        }
+
+        // if password does not matches to the pattern
+        // it will display an error message "Password is too weak"
+        else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            binding.etPasswordLayout.setError("Password is too weak");
+            return false;
+        } else {
+            binding.etPasswordLayout.setError(null);
+            return true;
+        }
+    }
+}
---
+++ app/src/main/java/com/mehboob/azdawaaj/ui/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.mehboob.azdawaaj.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.mehboob.azdawaaj.R;
+
+
+
+public class SplashActivity extends AppCompatActivity {
+private Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+
+    }
+}
